ebccdb3c2fc0d184c2a251c8cb9257fc3d486312
github username <github email address>
2018-08-08 23:42:12 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -8,6 +9,61 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(new_name)
+    song = self.new
+    song.name = new_name
+    song
+  end
+
+  def self.create_by_name(other_new_name)
+    song = self.new
+    song.name = other_new_name
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find{|song|
+        song.name == song_name
+      }
+  end
+
+  def self.find_or_create_by_name(song)
+    if self.find_by_name(song) == nil
+    self.create_by_name(song)
+    else
+    self.find_by_name(song)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song|
+    song.name
+    }
+  end
+
+  def self.new_from_filename(filename)
+    song = self.new
+    song.artist_name = filename.split(" - ")[0]
+    song.name = filename.split(" - ")[1].split(".")[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    self.all << song
+  end
+
+  def self.destroy_all
+    @@all = []
   end
 
 end
